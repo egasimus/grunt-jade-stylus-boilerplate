1145ef9a9db04de85f2b41b7d4e8b1c262d0e675
Adam Avramov <fallenblood@gmail.com>
2016-02-02 17:09:13 +0200

    update versions of things, enable live reloading, fix indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,80 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-		/*
-			TASKS
-		*/
+    /*
+      TASKS
+    */
 
-        copy: {
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/fonts/',
-                        src: ['**/**'],
-                        dest: 'stage/fonts'
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/images/',
-                        src: ['**/**'],
-                        dest: 'stage/images'
-                    },
-                    {
-                        expand: true,
-                        cwd: 'src/scripts/',
-                        src: ['**/**.js'],
-                        dest: 'stage/scripts'
-                    }
-                ]
-            }
-        },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/fonts/',
+            src: ['**/**'],
+            dest: 'stage/fonts'
+          },
+          {
+            expand: true,
+            cwd: 'src/images/',
+            src: ['**/**'],
+            dest: 'stage/images'
+          },
+          {
+            expand: true,
+            cwd: 'src/scripts/',
+            src: ['**/**.js'],
+            dest: 'stage/scripts'
+          }
+        ]
+      }
+    },
 
-        stylus: {
-            compile: {
-                files: [
-                    {
-                        src: 'src/styles/style.styl',
-                        dest: 'stage/styles/style.css'
-                    }
-                ]
-            }
-        },
+    stylus: {
+      compile: {
+        files: [
+          {
+            src: 'src/styles/style.styl',
+            dest: 'stage/styles/style.css'
+          }
+        ]
+      }
+    },
 
-        jade: {
-            compile: {
-                expand: true,
-                cwd: 'src/pages/',
-                src: '**/*.jade',
-                dest: 'stage/',
-                ext: '.html',
-            }
-        },
+    jade: {
+      compile: {
+        expand: true,
+        cwd: 'src/pages/',
+        src: '**/*.jade',
+        dest: 'stage/',
+        ext: '.html',
+      }
+    },
 
-        watch: {
-            files: ['src/**/*'],
-            tasks: ['stylus:compile', 'jade:compile', 'copy:main'],
-		},
+    watch: {
+      files: ['src/**/*'],
+      tasks: ['stylus:compile', 'jade:compile', 'copy:main'],
+      options: { livereload: true }
+    },
 
-        connect: {
-            server: {
-                options: {
-                    port: 9001,
-                    base: 'stage'
-                }
-            }
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'stage'
         }
-	});
+      }
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.loadNpmTasks('grunt-contrib-stylus');
-    grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-connect')
 
-    grunt.registerTask('default', ['connect:server', 'watch'])
-    grunt.registerTask('build', ['stylus', 'jade', 'copy:main']);
+  grunt.registerTask('default', ['connect:server', 'watch'])
+  grunt.registerTask('build', ['stylus', 'jade', 'copy:main']);
 };
